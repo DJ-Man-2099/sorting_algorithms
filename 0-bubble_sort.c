01133d0301ca37aef78ed8c2ad2ec775592bf0ac
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-16 23:32:58 +0200

    Task 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,13 +16,16 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	for (i = 0; i < size - 1; i++)
-		for (j = 1; j < size - i; j++)
-			if (array[j - 1] > array[j])
-			{
-				temp = array[j];
-				array[j] = array[j - 1];
-				array[j - 1] = temp;
-				print_array(array, size);
-			}
+	if (size >= 2)
+	{
+		for (i = 0; i < size - 1; i++)
+			for (j = 1; j < size - i; j++)
+				if (array[j - 1] > array[j])
+				{
+					temp = array[j];
+					array[j] = array[j - 1];
+					array[j - 1] = temp;
+					print_array(array, size);
+				}
+	}
 }
