7c98206baf91fb623fa9a003ac902b680a955ada
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-11-16 23:20:54 +0200

    Delete tests.h

--- tests.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <CUnit/Basic.h>
-#include "sort.h"
-void test_bubble_sort(void);
-void test_insertion_sort(void);
-void test_selection_sort(void);
