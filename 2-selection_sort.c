9033e943e4cf29ae13525b93940be388cb2f427e
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-16 23:56:06 +0200

    Task 3

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -19,6 +19,7 @@ void selection_sort(int *array, size_t size)
 		for (i = 0; i < size - 1; i++)
 		{
 			min = array[i];
+			min_j = i;
 			for (j = i + 1; j < size; j++)
 			{
 				if (array[j] < min)
