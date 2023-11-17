de75169e8294e6a570a55e30cdf3e356cd04e435
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-17 11:38:32 +0200

    Task 2, Please?

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -17,31 +17,31 @@ void insertion_sort_list(listint_t **list)
 	if (*list == NULL || (*list)->next == NULL)
 		return;
 	temp = *list;
-	current_compare = (*list)->next;
-	while (current_compare != NULL)
+	while (temp != NULL)
 	{
-		while (temp != NULL &&
-			   current_compare->n < temp->n)
+		while (temp->next != NULL &&
+			   temp->next->n < temp->n)
 		{
+			current_compare = temp->next;
 			temp->next = current_compare->next;
 			current_compare->prev = temp->prev;
+
 			if (temp->prev != NULL)
 				temp->prev->next = current_compare;
+
 			if (current_compare->next != NULL)
 				current_compare->next->prev = temp;
-			current_compare->next = temp;
+
 			temp->prev = current_compare;
+			current_compare->next = temp;
+
 			if (current_compare->prev == NULL)
-			{
 				*list = current_compare;
-				print_list(*list);
-				break;
-			}
+			else
+				temp = current_compare->prev;
+
 			print_list(*list);
-			current_compare = current_compare->prev;
-			temp = current_compare->prev;
 		}
-		temp = current_compare;
-		current_compare = current_compare->next;
+		temp = temp->next;
 	}
 }
