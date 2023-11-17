110e8ea49de064d07f867918ae88a79bdb91b8c2
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-17 11:45:11 +0200

    Task 2, WT......?

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,51 @@
 #include "sort.h"
-#include <stdio.h>
-
 /**
- * insertion_sort_list - sorts a DLL of integers in
- * ascending order using the insertion sort
- * algorithm
+ * insertion_sort_list - Sorting function
  *
- * @list: doubly linked list
- * Return: no return
+ * @list: list to sort
+ *
+ * sorts a doubly linked list of integers
+ * in ascending order
+ * using the Insertion sort algorithm
+ *
+ * Return: void
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *current_compare;
+	listint_t *new_list = *list,
+			  *current_compare,
+			  *temp;
 
-	if (!list)
+	if (list == NULL || *list == NULL ||
+		(*list)->next == NULL)
 		return;
-
 	temp = *list;
-
-	while (temp)
+	current_compare = (*list)->next;
+	while (current_compare != NULL)
 	{
-		while (temp->next && (temp->n > temp->next->n))
+		while (temp != NULL &&
+			   current_compare->n < temp->n)
 		{
-			current_compare = temp->next;
 			temp->next = current_compare->next;
 			current_compare->prev = temp->prev;
-
-			if (temp->prev)
+			if (temp->prev != NULL)
 				temp->prev->next = current_compare;
-
-			if (current_compare->next)
+			if (current_compare->next != NULL)
 				current_compare->next->prev = temp;
-
-			temp->prev = current_compare;
 			current_compare->next = temp;
-
-			if (current_compare->prev)
-				temp = current_compare->prev;
-			else
-				*list = current_compare;
-
-			print_list(*list);
+			temp->prev = current_compare;
+			if (current_compare->prev == NULL)
+			{
+				new_list = current_compare;
+				print_list(new_list);
+				break;
+			}
+			print_list(new_list);
+			current_compare = current_compare->prev;
+			temp = current_compare->prev;
 		}
-		temp = temp->next;
+		temp = current_compare;
+		current_compare = current_compare->next;
 	}
+	*list = new_list;
 }
