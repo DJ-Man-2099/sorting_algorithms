8b540fb3b21a58118ec31491e802ef0a40630aa5
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-17 22:41:01 +0200

    Task 4 with Test Cases

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -21,7 +21,7 @@ int lomuto_partitioner(int *array, int low, int high, size_t orig_size)
 		if (array[j] < pivot)
 		{
 			i++;
-			if (i != j)
+			if (i != j && array[j] != array[i])
 			{
 				temp = array[j];
 				array[j] = array[i];
@@ -30,7 +30,7 @@ int lomuto_partitioner(int *array, int low, int high, size_t orig_size)
 			}
 		}
 	}
-	if (i + 1 != high)
+	if (i + 1 != high && array[high] != array[i + 1])
 	{
 		temp = array[high];
 		array[high] = array[i + 1];
@@ -39,6 +39,20 @@ int lomuto_partitioner(int *array, int low, int high, size_t orig_size)
 	}
 	return (i + 1);
 }
+/**
+ * inner_quick_sort - Sorting function
+ *
+ * @array: array to partition
+ * @low: start
+ * @high: end
+ * @orig_size: full size
+ *
+ * sorts an array of integers in ascending order
+ * recursively
+ * using the Quick sort algorithm
+ *
+ * Return: void
+ */
 void inner_quick_sort(int *array, int low, int high, size_t orig_size)
 {
 	int pi;
