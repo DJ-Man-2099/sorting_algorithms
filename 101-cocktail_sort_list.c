e9df28c1e0343ccbcfee9d0c7d56f4064d204632
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-19 00:28:09 +0200

    Optional Task 2

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,15 +1,15 @@
 #include "sort.h"
 /**
- * swap - List Function
+ * swap2 - List Function
  *
  * @a: 1st node
  * @b: 2nd node
  *
- * swaps a and b
+ * swap2s a and b
  *
  * Return: void
  */
-void swap(listint_t **a, listint_t **b)
+void swap2(listint_t **a, listint_t **b)
 {
 	(*a)->next = (*b)->next;
 	(*b)->prev = (*a)->prev;
@@ -33,51 +33,50 @@ void swap(listint_t **a, listint_t **b)
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *new_list, *forward_compare, *forward_start,
+	listint_t *forward_compare, *forward_start,
 		*reverse_compare, *reverse_start, *temp;
 	int forward = 0, i;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	new_list = *list;
 	forward_start = *list;
-	while (forward_start != reverse_start)
+	for (; forward_start != reverse_start; forward++)
 	{
 		for (i = 0; i < forward; i++)
 			forward_start = forward_start->next;
+		if (forward_start != reverse_start)
+			break;
 		forward_compare = forward_start;
 		temp = forward_start->next;
 		while (temp != NULL)
 		{
 			if (temp != NULL && temp->n < forward_compare->n)
 			{
-				swap(&forward_compare, &temp);
+				swap2(&forward_compare, &temp);
 				if (temp->prev == NULL)
-					new_list = temp;
-				print_list(new_list);
+					*list = temp;
+				print_list(*list);
 			}
 			forward_compare = temp;
 			temp = temp->next;
 		}
 		reverse_start = forward_compare->prev;
-		for (i = 0; i < forward; i++)
+		for (i = 0; i < forward + 1; i++)
 			reverse_start = reverse_start->prev;
-		forward++;
 		reverse_compare = reverse_start;
 		temp = reverse_start->prev;
 		while (temp != NULL)
 		{
 			if (temp != NULL && temp->n > reverse_compare->n)
 			{
-				swap(&temp, &reverse_compare);
+				swap2(&temp, &reverse_compare);
 				if (reverse_compare->prev == NULL)
-					new_list = reverse_compare;
-				print_list(new_list);
+					*list = reverse_compare;
+				print_list(*list);
 			}
 			reverse_compare = temp;
 			temp = temp->prev;
 		}
 		forward_start = reverse_compare->next;
 	}
-	*list = new_list;
 }
